2b17131a40da5c357c289b01c16453185a52dc34
= <=>
2022-07-18 16:21:35 -0500

    agregando modo desarrollo

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -2,17 +2,15 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const CopyPlugin = require('copy-webpack-plugin');
-const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
-const TerserPlugin = require('terser-webpack-plugin');
 const Dotenv = require('dotenv-webpack');
 
 module.exports = {
-    mode: 'development',
     entry: './src/index.js',
     output: {
     path: path.resolve(__dirname, 'dist'),
     filename: '[name].[contenthash].js',
     },
+    mode: 'development',
     resolve: {
         extensions: ['.js'],
         alias: {
@@ -61,11 +59,4 @@ module.exports = {
         }),
         new Dotenv(),
     ],
-    optimization: {
-        minimize: true,
-        minimizer: [
-            new CssMinimizerPlugin(),
-            new TerserPlugin(),
-        ]
-    }
 };
